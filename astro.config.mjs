893d78a6e6a1434f336883180dfe21b564197833
amanimavu <amanimavu@gmail.com>
2026-09-18 16:38:02 +0300

    feat: add SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://biodiesel.goldentie.africa/",
+    fonts: [
+        {
+            provider: fontProviders.fontsource(),
+            name: "Montserrat",
+            cssVariable: "--font-montserrat",
+            weights: [500, 600, 700, 800],
+        },
+        {
+            provider: fontProviders.fontsource(),
+            name: "Lora",
+            cssVariable: "--font-lora",
+            weights: [400, 500, 600],
+        },
+    ],
+});
