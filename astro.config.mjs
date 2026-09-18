09135e92561a290d178aa0cb0f166882a48650e6
amanimavu <amanimavu@gmail.com>
2026-09-18 16:59:21 +0300

    build: configure clodflare CI/CD deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,26 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://biodiesel.goldentie.africa/",
-    fonts: [
-        {
-            provider: fontProviders.fontsource(),
-            name: "Montserrat",
-            cssVariable: "--font-montserrat",
-            weights: [500, 600, 700, 800],
-        },
-        {
-            provider: fontProviders.fontsource(),
-            name: "Lora",
-            cssVariable: "--font-lora",
-            weights: [400, 500, 600],
-        },
-    ],
+  site: "https://biodiesel.goldentie.africa/",
+
+  fonts: [
+      {
+          provider: fontProviders.fontsource(),
+          name: "Montserrat",
+          cssVariable: "--font-montserrat",
+          weights: [500, 600, 700, 800],
+      },
+      {
+          provider: fontProviders.fontsource(),
+          name: "Lora",
+          cssVariable: "--font-lora",
+          weights: [400, 500, 600],
+      },
+  ],
+
+  adapter: cloudflare(),
 });
